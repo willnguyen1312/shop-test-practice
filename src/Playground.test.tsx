d272910b6531b9ad9896029e075047fc5aa68378
Nam Nguyen <nam.nguyen@shopify.com>
2024-01-17 09:56:13 -0400

    Add async test example 😉

---
+++ src/Playground.test.tsx
@@ -0,0 +1,40 @@
+import { PolarisTestProvider } from "@shopify/polaris";
+import { mount } from "@shopify/react-testing";
+import "@shopify/react-testing/matchers";
+
+import React from "react";
+import { expect, test } from "vitest";
+
+const Playground = () => {
+  const [third, setThird] = React.useState(0);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setThird(1 + 2);
+    }, 0);
+  }, []);
+
+  return (
+    <div>
+      <h1>Playground</h1>
+      <p role="status">Third: {third}</p>
+    </div>
+  );
+};
+
+function renderApp() {
+  return mount(
+    <PolarisTestProvider>
+      <Playground />
+    </PolarisTestProvider>,
+  );
+}
+
+test("Playground component should render successfully", async () => {
+  const wrapper = renderApp();
+
+  expect(wrapper).toContainReactText("Third: 0");
+
+  //   How to test async code with @shopify/react-testing? 😅
+  //   expect(wrapper).toContainReactText("Third: 3");
+});
