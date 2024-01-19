cedb3f38a42771491fd5ee8c17fe2e5ff5cc7958
Nam Nguyen <nam.nguyen.node@gmail.com>
2024-01-19 09:58:54 -0400

    Wonderful 💞

--- src/Playground-rtl.test.tsx
+++ src/Playground-rtl.test.tsx
@@ -1,6 +1,6 @@
-import { render } from "@testing-library/react";
-import { checkA11y } from "./testUtils";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { checkA11y } from "./testUtils";
 
 test("Playground component should render successfully", async () => {
   const wrapper = render(
@@ -13,7 +13,7 @@ test("Playground component should render successfully", async () => {
   await checkA11y(wrapper.container);
 });
 
-test("events do buble in rtl 😍", async () => {
+test("events do bubble in rtl 😍", async () => {
   let divClick = false;
   let buttonClick = false;
 
@@ -40,4 +40,42 @@ test("events do buble in rtl 😍", async () => {
 
   expect(divClick).toBe(true);
   expect(buttonClick).toBe(true);
+});
+
+test.only("events do bubble in rtl and more 😍", async () => {
+  let divClick = false;
+  let buttonClick = false;
+
+  const user = userEvent.setup();
+  const wrapper = render(
+    <div
+      data-testid="div"
+      onClick={() => {
+        divClick = true;
+      }}
+    >
+      <button
+        onClick={() => {
+          buttonClick = true;
+        }}
+      >
+        Click me
+      </button>
+    </div>,
+  );
+
+  const button = screen.getByText("Click me");
+  const clickEvent = new MouseEvent("click", { bubbles: true });
+
+  const div = screen.getByTestId("div");
+  div.addEventListener("click", (event) => {
+    console.log("div event ", event === clickEvent);
+  });
+
+  button.dispatchEvent(clickEvent);
+
+  // await user.click(button);
+
+  // expect(divClick).toBe(true);
+  // expect(buttonClick).toBe(true);
 });
